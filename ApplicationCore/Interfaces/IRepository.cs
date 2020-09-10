03e79fec8d382e15069b9aa38c3a68602509d679
Aomiki <aomiki.milky@icloud.com>
2020-09-10 13:51:45 +0400

    Defined IRepository and IMenuService

---
+++ ApplicationCore/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Entities;
+using System.Collections.Generic;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IRepository<T> where T: MenuItem
+    {
+        T GetById(int id);
+        List<T> ListAll();
+        void Add(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+
+        
+    }
+}
