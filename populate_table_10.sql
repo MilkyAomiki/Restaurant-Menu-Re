e8a830e9db626e8eb6f09b8e22f54f0b212fd413
Aomiki <aomiki.milky@icloud.com>
2020-09-18 23:13:15 +0400

    File for populating database

---
+++ populate_table_10.sql
@@ -0,0 +1,23 @@
+USE Menu;
+
+INSERT INTO MenuItem(title, ingredients, description, price, grams, calories, cooking_time) 
+			VALUES('Cinnamon roll', 'Delicious pastry', 'Cinnamon, flour', 2, 1, 12, 1);
+INSERT INTO MenuItem(title, ingredients, description, price, grams, calories, cooking_time) 
+			VALUES('Ground beef Gyros', 'This is a great summer meal', 'yogurt, lean ground beef...', 36, 30, 120, 22);
+INSERT INTO MenuItem(title, ingredients, description, price, grams, calories, cooking_time) 
+			VALUES('Lasagna', 'This is a very meaty lasagna.', 'lean ground beef, Italian sausage...', 10, 40, 100, 150);
+INSERT INTO MenuItem(title, ingredients, description, price, grams, calories, cooking_time) 
+			VALUES('Chinese fried rice', 'Has the flavor those other recipes are missing. Tastes like takeout', 'onion, oil...', 21, 32, 110, 28);
+INSERT INTO MenuItem(title, ingredients, description, price, grams, calories, cooking_time) 
+			VALUES('Grandmas Sloppy joes', 'The perfect comfort food!', 'hamburger, onion, tomato sauce...',5, 20, 50, 35);
+INSERT INTO MenuItem(title, ingredients, description, price, grams, calories, cooking_time) 
+			VALUES('Famous Challah', 'Special bread in Jewish cuisine', 'flour, dry yeast...', 20, 12, 15, 215);
+INSERT INTO MenuItem(title, ingredients, description, price, grams, calories, cooking_time) 
+			VALUES('Sweet & Sour Chicken', 'Great chicken with diverse flavor', 'soy sauce, sherry wine', 30, 70, 80, 100);
+INSERT INTO MenuItem(title, ingredients, description, price, grams, calories, cooking_time) 
+			VALUES('Lemon baked cod', 'Great baked cod dish', 'cod fish fillet, butter...', 24, 30, 29, 35);
+INSERT INTO MenuItem(title, ingredients, description, price, grams, calories, cooking_time) 
+			VALUES('The Perfect Burger', 'This will be the best burger you ever had', 'egg, mustard...', 21, 32, 70, 30)
+INSERT INTO MenuItem(title, ingredients, description, price, grams, calories, cooking_time) 
+			VALUES('Chicken pot pie', 'Great on a cold winter day', 'potato, onion, celery...', 26, 60, 56, 105);
+
