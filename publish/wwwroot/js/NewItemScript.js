beee0e1255e1f0dce1ba548b726ced6aacb3fd34
Aomiki <aomiki.milky@icloud.com>
2020-10-30 20:02:46 +0400

    General improvements

--- publish/wwwroot/js/NewItemScript.js
+++
@@ -1,7 +0,0 @@
-﻿var addClickEvent = function () {
-    document.getElementById('cancel-button').onclick = function () {
-        window.location.href = "/menu";
-    }
-}
-
-window.onload = addClickEvent;
