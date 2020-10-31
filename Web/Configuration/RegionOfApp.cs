c1efe8525d172e6aa3b7af10bbddeda6604f0728
Aomiki <aomiki.milky@icloud.com>
2020-10-31 17:29:17 +0400

    Fixes

--- Web/Configuration/RegionOfApp.cs
+++ Web/Configuration/RegionOfApp.cs
@@ -4,7 +4,7 @@ namespace Web.Configuration
 {
     public sealed class RegionOfApp
     {
-        public static RegionOfApp Instance { get; } = new RegionOfApp();
+        public static readonly RegionOfApp Instance = new RegionOfApp();
         public RegionInfo Current { get; } = new RegionInfo("en-US");
 
         static RegionOfApp()
